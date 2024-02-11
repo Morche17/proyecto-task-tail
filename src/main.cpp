0bd6821e0e76fdfdcfc003e35e46d9ab334f3e3d
Emanuel Tavares <tavares2003@tutanota.com>
2024-02-10 18:04:07 -0800

    Librería raylib con la que se trabaja el proyecto.

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <raylib.h>
+
+int main(void)
+{
+
+  // Una ventana básica con un texto normal
+  InitWindow(800, 450, "Task Tail 0.0.1");
+
+  while(!WindowShouldClose())
+  {
+    BeginDrawing();
+      ClearBackground(RAYWHITE);
+      DrawText("Aquí comienza la creación del zorro tareas cola digital.", 190, 200, 20, BLACK);
+    EndDrawing();
+  }
+
+  CloseWindow();
+
+  return 0;
+}
